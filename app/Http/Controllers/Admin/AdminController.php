765bc77396b266670dffbde5afc8762779990e7c
kamrul1275 <kamrul1275.info@gmail.com>
2023-12-05 22:31:52 +0600

    admin login,logout done

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    function AdminDashboard(){
+        return view('Backend.layout.dashboard');
+   }//end method
+
+
+   public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
